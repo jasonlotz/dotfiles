008e7b5b33a2670df9d4bd7603721d049215de8c
jasonlotz <jasonlotz@gmail.com>
2024-01-15 20:53:43 -0500

    Addec whichkey.

--- lua/jason/plugins/init.lua
+++ lua/jason/plugins/init.lua
@@ -1,6 +1,6 @@
 -- init plugins that don't require additional config
 return {
-   "nvim-lua/plenary.nvim", -- lua functions that many plugins use
+  "nvim-lua/plenary.nvim", -- lua functions that many plugins use
 
   "christoomey/vim-tmux-navigator", -- tmux & split window navigation
 
