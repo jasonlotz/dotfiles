d77d264dbf2bdec1cba4dfe7919a5f815f0740d0
jasonlotz <jasonlotz@gmail.com>
2024-01-17 22:59:42 -0500

    Add simple script to execute stow commands.

---
+++ stow-dotfiles.sh
@@ -0,0 +1,3 @@
+stow -v --dir=$HOME/Workspaces/dotfiles/nvim/ --target=$HOME/ --stow /
+stow -v --dir=$HOME/Workspaces/dotfiles/tmux/ --target=$HOME/ --stow / 
+stow -v --dir=$HOME/Workspaces/dotfiles/zsh/ --target=$HOME/ --stow / 
