0a82ec81267cee65c406e6b20b3857ae0d19a4a6
jasonlotz <jasonlotz@gmail.com>
2024-01-20 16:53:13 -0500

    Added tokyonight colorscheme.

--- nvim/.config/nvim/lua/jason/plugins/colorschemes/kanagawa.lua
+++ nvim/.config/nvim/lua/jason/plugins/colorschemes/kanagawa.lua
@@ -2,8 +2,7 @@ return {
   {
     "rebelot/kanagawa.nvim",
     config = function()
-      -- load the colorscheme here
-      -- vim.cmd([[colorscheme kanagawa]])
+      vim.cmd([[colorscheme kanagawa]])
     end,
   },
 }
--- nvim/.config/nvim/lua/jason/plugins/colorschemes/sonokai.lua
+++ nvim/.config/nvim/lua/jason/plugins/colorschemes/sonokai.lua
@@ -3,8 +3,7 @@ return {
     "sainnhe/sonokai",
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme sonokai]])
+      -- vim.cmd([[colorscheme sonokai]])
     end,
   },
 }
