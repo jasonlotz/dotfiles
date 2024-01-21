f108799fef19197bd467a112b24c6d6fd61e88a2
jasonlotz <jasonlotz@gmail.com>
2024-01-21 12:46:18 -0500

    Updated a couple commands (Flash toggle, LSP hover). Updated plugins to latest versions.

--- nvim/.config/nvim/lua/jason/plugins/auto-save.lua
+++ nvim/.config/nvim/lua/jason/plugins/auto-save.lua
@@ -1,10 +1,10 @@
 return {
   "okuuva/auto-save.nvim",
   cmd = "ASToggle", -- optional for lazy loading on command
-  event = { "TextChanged" }, -- optional for lazy loading on trigger events
+  event = { "InsertLeave", "TextChanged" }, -- optional for lazy loading on trigger events
   opts = {
     trigger_events = { -- See :h events
-      immediate_save = { "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
+      immediate_save = { "InsertLeave", "BufLeave", "FocusLost" }, -- vim events that trigger an immediate save
       defer_save = { "TextChanged" }, -- vim events that trigger a deferred save (saves after `debounce_delay`)
       cancel_defered_save = { "InsertEnter" }, -- vim events that cancel a pending deferred save
     },
