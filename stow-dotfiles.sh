a3a1f01339d76e25fd8864eae58789ebe081944e
jasonlotz <jasonlotz@gmail.com>
2024-01-22 09:29:15 -0500

    Fixed typo.

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -2,4 +2,4 @@ stow -v --dir=$HOME/Workspaces/dotfiles/nvim/ --target=$HOME/ --stow /
 stow -v --dir=$HOME/Workspaces/dotfiles/tmux/ --target=$HOME/ --stow / 
 stow -v --dir=$HOME/Workspaces/dotfiles/zsh/ --target=$HOME/ --stow / 
 stow -v --dir=$HOME/Workspaces/dotfiles/yabai/ --target=$HOME/ --stow /
-stow -v --dir=$HOME/Workspaces/dotfiles/skhkd/ --target=$HOME/ --stow /
+stow -v --dir=$HOME/Workspaces/dotfiles/skhd/ --target=$HOME/ --stow /
