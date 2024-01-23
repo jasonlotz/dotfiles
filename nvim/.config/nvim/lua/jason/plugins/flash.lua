d838816f70fb3848457cdd504f84af27a1da310b
jasonlotz <jasonlotz@gmail.com>
2024-01-23 11:00:19 -0500

    Added nvim-surround plugin.

--- nvim/.config/nvim/lua/jason/plugins/flash.lua
+++ nvim/.config/nvim/lua/jason/plugins/flash.lua
@@ -5,10 +5,10 @@ return {
   opts = {},
   -- stylua: ignore
   keys = {
-    { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Search" },
-    { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+    { "m", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Search" },
+    { "M", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
     { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
     { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-    { "<c-F>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    { "<c-F>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle flash Search" },
   },
 }
