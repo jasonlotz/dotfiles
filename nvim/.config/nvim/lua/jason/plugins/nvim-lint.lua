1e565f21ee37a5f6bfb35c9d3c794404178734ca
jasonlotz <jasonlotz@gmail.com>
2024-01-26 09:40:31 -0500

    Setup lua linter.

--- nvim/.config/nvim/lua/jason/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/jason/plugins/nvim-lint.lua
@@ -11,6 +11,7 @@ return {
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       python = { "pylint" },
+      lua = { "luacheck" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
