30054466f01182e04b3f185d74f7dba59002a37f
jasonlotz <jasonlotz@gmail.com>
2024-01-27 00:53:55 -0500

    Updated nvim-notify configuration to add a keybind for closing all notifications.

--- nvim/.config/nvim/lua/jason/plugins/nvim-notify.lua
+++ nvim/.config/nvim/lua/jason/plugins/nvim-notify.lua
@@ -1,16 +1,38 @@
 return {
   "rcarriga/nvim-notify",
   event = "BufRead",
-  config = function()
-    require("notify").setup({
-      -- Animation style
-      stages = "fade_in_slide_out",
+  config = {
+    -- Animation style
+    stages = "fade_in_slide_out",
 
-      -- Default timeout for notifications
-      timeout = 5000,
+    -- Default timeout for notifications
+    timeout = 3000,
 
-      -- For stages that change opacity this is treated as the highlight behind the window
-      background_colour = "#000000",
-    })
-  end,
+    -- For stages that change opacity this is treated as the highlight behind the window
+    background_colour = "#000000",
+
+    -- Max height
+    max_height = function()
+      return math.floor(vim.o.lines * 0.75)
+    end,
+
+    -- Max width
+    max_width = function()
+      return math.floor(vim.o.columns * 0.75)
+    end,
+
+    -- Default layout config
+    on_open = function(win)
+      vim.api.nvim_win_set_config(win, { zindex = 100 })
+    end,
+  },
+  keys = {
+    {
+      "<leader>un",
+      function()
+        require("notify").dismiss({ silent = true, pending = true })
+      end,
+      desc = "Dismiss all notifications",
+    },
+  },
 }
