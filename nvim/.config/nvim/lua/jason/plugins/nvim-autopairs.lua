9bcafc6cbbecc4e73728afc674176da582128df2
jasonlotz <jasonlotz@gmail.com>
2024-01-27 17:15:01 -0500

    Reverting change to no longer use config and opts together. Unless needed, only use one or the other.

--- nvim/.config/nvim/lua/jason/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/jason/plugins/nvim-autopairs.lua
@@ -4,15 +4,20 @@ return {
   dependencies = {
     "hrsh7th/nvim-cmp",
   },
-  opts = {
-    check_ts = true, -- enable treesitter
-    ts_config = {
-      lua = { "string" }, -- don't add pairs in lua string treesitter nodes
-      javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
-      java = false, -- don't check treesitter on java
-    },
-  },
   config = function()
+    -- import nvim-autopairs
+    local autopairs = require("nvim-autopairs")
+
+    -- configure autopairs
+    autopairs.setup({
+      check_ts = true, -- enable treesitter
+      ts_config = {
+        lua = { "string" }, -- don't add pairs in lua string treesitter nodes
+        javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
+        java = false, -- don't check treesitter on java
+      },
+    })
+
     -- import nvim-autopairs completion functionality
     local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 
