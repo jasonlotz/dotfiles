8fdab803b9a59b74d2c191ed2e8e2ea87718c276
jasonlotz <jasonlotz@gmail.com>
2024-01-28 00:37:00 -0500

    Added symbols-outline plug-in.

---
+++ nvim/.config/nvim/lua/jason/plugins/symbols-outline.lua
@@ -0,0 +1,8 @@
+return {
+  "simrat39/symbols-outline.nvim",
+  cmd = { "SymbolsOutline" },
+  config = true,
+  keys = {
+    { "<leader>cs", "<cmd>SymbolsOutline<CR>", desc = "Symbols outline" },
+  },
+}
