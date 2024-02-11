626af23917faecf075923d5cfda3f217ccc5bbc9
jasonlotz <jasonlotz@gmail.com>
2024-02-11 11:27:08 -0500

    Removed hardtime plug-in.

--- nvim/.config/nvim/lua/jason/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "m4xshen/hardtime.nvim",
-  dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-  opts = {
-    max_count = 5,
-    disable_mouse = false,
-    enabled = true,
-  },
-}
