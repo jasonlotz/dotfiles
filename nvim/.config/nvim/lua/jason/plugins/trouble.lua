92fcfdeccdf87264c0a3059ff67e52fd55707db6
jasonlotz <jasonlotz@gmail.com>
2024-02-18 15:09:44 -0500

    Updated todo-comment and trouble plug-ins to use VeryLazy loading

--- nvim/.config/nvim/lua/jason/plugins/trouble.lua
+++ nvim/.config/nvim/lua/jason/plugins/trouble.lua
@@ -1,6 +1,7 @@
 return {
   "folke/trouble.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  event = "VeryLazy",
   opts = {},
   keys = {
     { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
