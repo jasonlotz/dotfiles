7ea45d8f629a5ecbd9153413ca5f0e0b69bdfa34
jasonlotz <jasonlotz@gmail.com>
2024-02-20 21:37:57 -0500

    Fix deprecated trouble keymaps

--- nvim/.config/nvim/lua/jason/plugins/trouble.lua
+++ nvim/.config/nvim/lua/jason/plugins/trouble.lua
@@ -4,8 +4,8 @@ return {
   opts = {},
   keys = {
     { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble" },
-    { "<leader>xw", "<cmd>TroubleToggle lsp_workspace_diagnostics<cr>", desc = "Toggle Trouble Workspace Diagnostics" },
-    { "<leader>xd", "<cmd>TroubleToggle lsp_document_diagnostics<cr>", desc = "Toggle Trouble Document Diagnostics" },
+    { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Toggle Trouble Workspace Diagnostics" },
+    { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Toggle Trouble Document Diagnostics" },
     { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Toggle Trouble Location List" },
     { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Toggle Trouble Quickfix" },
     { "<leader>xr", "<cmd>TroubleToggle lsp_references<cr>", desc = "Toggle Trouble LSP References" },
