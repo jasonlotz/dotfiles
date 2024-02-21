fa5e6c57861eebe90c4c9c021e66b74cdfbd6148
jasonlotz <jasonlotz@gmail.com>
2024-02-21 15:39:08 -0500

    Standardize more keyaps

--- nvim/.config/nvim/lua/jason/core/keymaps.lua
+++ nvim/.config/nvim/lua/jason/core/keymaps.lua
@@ -4,45 +4,45 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for conciseness
 
 -- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-keymap.set("v", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "jk", "<esc>", { desc = "Exit insert mode with jk" })
+keymap.set("v", "jk", "<esc>", { desc = "Exit insert mode with jk" })
 
 -- clear search highlights
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>nh", ":nohl<cr>", { desc = "Clear search highlights" })
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
 
 -- quick save
-keymap.set("n", "<leader>w", ":w<CR>", { desc = "Quick save" })
-keymap.set("n", "<leader>ww", ":wa<CR>", { desc = "Quick save all" })
+keymap.set("n", "<leader>w", ":w<cr>", { desc = "Quick save" })
+keymap.set("n", "<leader>ww", ":wa<cr>", { desc = "Quick save all" })
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap.set("n", "<leader>+", "<c-a>", { desc = "Increment number" }) -- increment
+keymap.set("n", "<leader>-", "<c-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<c-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<c-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<c-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>sx", "<cmd>close<cr>", { desc = "Close current split" }) -- close current split window
 
 -- tab management
-keymap.set("n", "<leader>tt", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>to", "<cmd>tabonly<CR>", { desc = "Close all but current tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
-keymap.set("n", "<leader>q", "<cmd>q<CR>", { noremap = true, silent = true, desc = "Close current buffer" }) -- close current buffer
+keymap.set("n", "<leader>tt", "<cmd>tabnew<cr>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>to", "<cmd>tabonly<cr>", { desc = "Close all but current tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<cr>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<cr>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<cr>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<cr>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>q", "<cmd>q<cr>", { noremap = true, silent = true, desc = "Close current buffer" }) -- close current buffer
 
 -- move lines
-keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move line down" })
-keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move line up" })
+keymap.set("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move line down" })
+keymap.set("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move line up" })
 
 -- keep cursor center on half-page up/down and search
-keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move cursor half-page up" })
-keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move cursor half-page down" })
+keymap.set("n", "<c-u>", "<c-u>zz", { desc = "Move cursor half-page up" })
+keymap.set("n", "<c-d>", "<c-d>zz", { desc = "Move cursor half-page down" })
 keymap.set("n", "n", "nzzzv", { desc = "Move cursor to next search result" })
 keymap.set("n", "N", "Nzzzv", { desc = "Move cursor to previous search result" })
 
