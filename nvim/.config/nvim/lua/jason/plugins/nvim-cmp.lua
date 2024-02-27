ecd77e8bbab423673e30ce265b1555d0ac63f3ba
jasonlotz <jasonlotz@gmail.com>
2024-02-26 22:57:09 -0500

    feat: added auto completion for signature help

--- nvim/.config/nvim/lua/jason/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/jason/plugins/nvim-cmp.lua
@@ -43,6 +43,7 @@ return {
         { name = "luasnip" }, -- snippets
         { name = "buffer" }, -- text within current buffer
         { name = "path" }, -- file system paths
+        { name = "nvim_lsp_signature_help" }, -- signature help
       }),
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
