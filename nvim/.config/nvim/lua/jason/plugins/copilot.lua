43c68f2e29ae0dbe8f53120450232078fc534941
jasonlotz <jasonlotz@gmail.com>
2024-02-27 00:14:59 -0500

    feat: added a command and keymap for toggling copilot

--- nvim/.config/nvim/lua/jason/plugins/copilot.lua
+++ nvim/.config/nvim/lua/jason/plugins/copilot.lua
@@ -1,3 +1,17 @@
 return {
   "github/copilot.vim",
+  setup = function()
+    local copilot_on = true
+    vim.api.nvim_create_user_command("CopilotToggle", function()
+      if copilot_on then
+        vim.cmd("Copilot disable")
+        print("Copilot OFF")
+      else
+        vim.cmd("Copilot enable")
+        print("Copilot ON")
+      end
+      copilot_on = not copilot_on
+    end, { nargs = 0 })
+    vim.keymap.set("n", "<leader>co", ":CopilotToggle<CR>", { desc = "Toggle Copilot", noremap = true, silent = true })
+  end,
 }
