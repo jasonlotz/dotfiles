055b6de6d913a5d0c833d21067946bd8c65474f2
jasonlotz <jasonlotz@gmail.com>
2024-02-27 21:59:01 -0500

    fix: add depedency for nvim-treesitter-context

--- nvim/.config/nvim/lua/jason/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/jason/plugins/nvim-treesitter.lua
@@ -5,6 +5,7 @@ return {
     build = ":TSUpdate",
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
+      "nvim-treesitter/nvim-treesitter-context",
       "windwp/nvim-ts-autotag",
     },
     config = function()
