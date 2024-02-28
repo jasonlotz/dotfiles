2b17f40cae8f224b6f676f749e1f0e657794ba35
jasonlotz <jasonlotz@gmail.com>
2024-02-28 13:02:23 -0500

    feat: add neodev

--- nvim/.config/nvim/lua/jason/plugins/lsp/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/jason/plugins/lsp/nvim-lspconfig.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "hrsh7th/cmp-nvim-lsp",
     { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim", config = true },
   },
   config = function()
     -- import lspconfig plugin
