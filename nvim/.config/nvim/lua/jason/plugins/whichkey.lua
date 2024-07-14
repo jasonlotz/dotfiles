10af7002aa3cc2947f5a9b7ed62876c4a1140075
jasonlotz <jasonlotz@gmail.com>
2024-07-14 19:23:13 -0400

    fix: update whichkey config to resolve depracated warnings

--- nvim/.config/nvim/lua/jason/plugins/whichkey.lua
+++ nvim/.config/nvim/lua/jason/plugins/whichkey.lua
@@ -5,38 +5,31 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 300
   end,
-  config = function()
-    local wk = require("which-key")
-
-    wk.setup({
-      window = {
-        border = "single",
-      },
-      icons = {
-        breadcrumb = "»",
-        separator = "➜",
-        group = "+",
-      },
-    })
-
-    wk.register({
-      ["<leader>"] = {
-        c = "Code",
-        d = "Debug",
-        e = "File explorer",
-        f = "Fuzzy find",
-        g = "Git",
-        m = "Flash search",
-        n = "Swap next",
-        o = "Obsidian",
-        p = "Swap Previous",
-        r = "Replace",
-        s = "Split",
-        t = "Tab",
-        u = "Dismiss / clear",
-        w = "Save",
-        x = "Trouble",
-      },
-    })
-  end,
+  opts = {
+    window = {
+      border = "single",
+    },
+    icons = {
+      breadcrumb = "»",
+      separator = "➜",
+      group = "+",
+    },
+    keys = {
+      { "<leader>c", desc = "Code" },
+      { "<leader>d", desc = "Debug" },
+      { "<leader>e", desc = "File explorer" },
+      { "<leader>f", desc = "Fuzzy find" },
+      { "<leader>g", desc = "Git" },
+      { "<leader>m", desc = "Flash search" },
+      { "<leader>n", desc = "Swap next" },
+      { "<leader>o", desc = "Obsidian" },
+      { "<leader>p", desc = "Swap Previous" },
+      { "<leader>r", desc = "Replace" },
+      { "<leader>s", desc = "Split" },
+      { "<leader>t", desc = "Tab" },
+      { "<leader>u", desc = "Dismiss / clear" },
+      { "<leader>w", desc = "Save" },
+      { "<leader>x", desc = "Trouble" },
+    },
+  },
 }
