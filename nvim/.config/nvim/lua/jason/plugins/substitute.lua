079abd9082fe6b104adb5902add3c73e08da1f27
jasonlotz <jasonlotz@gmail.com>
2024-08-21 14:33:36 -0400

    fix: resolve issue with substitute operator not working

--- nvim/.config/nvim/lua/jason/plugins/substitute.lua
+++ nvim/.config/nvim/lua/jason/plugins/substitute.lua
@@ -2,9 +2,9 @@ return {
   "gbprod/substitute.nvim",
   config = true,
   keys = {
-    { mode = "n", "<leader>r", "<cmd>lua require('substitute').operator()<cr>", desc = "substitute" },
-    { mode = "n", "<leader>rr", "<cmd>lua require('substitute').line()<cr>", desc = "substitute" },
-    { mode = "n", "<leader>R", "<cmd>lua require('substitute').eol()<cr>", desc = "substitute-eol" },
-    { mode = "x", "<leader>r", "<cmd>lua require('substitute').visual()<cr>", desc = "substitute" },
+    { mode = "n", "<leader>r", "<cmd>lua require('substitute').operator()<cr>", desc = "substitute", noremap = true },
+    { mode = "n", "<leader>rr", "<cmd>lua require('substitute').line()<cr>", desc = "substitute line", noremap = true },
+    { mode = "n", "<leader>R", "<cmd>lua require('substitute').eol()<cr>", desc = "substitute-eol", noremap = true },
+    { mode = "x", "<leader>r", "<cmd>lua require('substitute').visual()<cr>", desc = "substitute", noremap = true },
   },
 }
