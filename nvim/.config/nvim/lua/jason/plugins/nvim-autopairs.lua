369b9e073460f58236c36e7b4eb26d8d1e7398e5
jasonlotz <jasonlotz@gmail.com>
2024-11-10 21:12:48 -0500

    feat: use autopairs on go files

--- nvim/.config/nvim/lua/jason/plugins/nvim-autopairs.lua
+++ nvim/.config/nvim/lua/jason/plugins/nvim-autopairs.lua
@@ -15,6 +15,7 @@ return {
         lua = { "string" }, -- don't add pairs in lua string treesitter nodes
         javascript = { "template_string" }, -- don't add pairs in javscript template_string treesitter nodes
         java = false, -- don't check treesitter on java
+        gopls = true,
       },
     })
 
