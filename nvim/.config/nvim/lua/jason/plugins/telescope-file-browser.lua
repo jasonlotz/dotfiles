c569e38d557ea8b7831cfda94d2a949f6476bdd2
jasonlotz <jasonlotz@gmail.com>
2024-12-27 09:58:05 -0500

    feat: update config for telescope file browser

--- nvim/.config/nvim/lua/jason/plugins/telescope-file-browser.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "nvim-telescope/telescope-file-browser.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  keys = {
-    { "<leader>fb", ":Telescope file_browser<cr>", desc = "Telescope file browser" },
-    { "<leader>fB", ":Telescope file_browser files=false<cr>", desc = "Telescope folder browser" },
-  },
-}
--- nvim/.config/nvim/lua/jason/plugins/telescope.lua
+++ nvim/.config/nvim/lua/jason/plugins/telescope.lua
@@ -6,6 +6,7 @@ return {
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     "nvim-tree/nvim-web-devicons",
     "nvim-telescope/telescope-ui-select.nvim",
+    "nvim-telescope/telescope-file-browser.nvim",
   },
   config = function()
     local telescope = require("telescope")
@@ -13,6 +14,7 @@ return {
 
     telescope.setup({
       pickers = {
+        file_browser = { theme = "ivy" },
         live_grep = {
           file_ignore_patterns = { "node_modules/", ".git/", ".venv/", ".obsidian/" },
           additional_args = function(_)
@@ -49,6 +51,7 @@ return {
     telescope.load_extension("fzf")
     telescope.load_extension("harpoon")
     telescope.load_extension("ui-select")
+    telescope.load_extension("file_browser")
   end,
   keys = {
     { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Fuzzy find files in cwd" },
@@ -63,5 +66,8 @@ return {
     { "<leader>fq", "<cmd>Telescope quickfix<cr>", desc = "Fuzzy find quickfix" },
     { "<leader>fQ", "<cmd>Telescope quickfixhistory<cr>", desc = "Fuzzy find quickfix history" },
     { "<leader>fR", "<cmd>Telescope registers<cr>", desc = "Fuzzy find registers" },
+    { "<leader>fb", "<cmd>Telescope file_browser<cr>", desc = "Telescope file browser" },
+    { "<leader>fB", "<cmd>Telescope file_browser files=false<cr>", desc = "Telescope folder browser" },
+    hidden = true,
   },
 }
