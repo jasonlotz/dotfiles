2c8c278fa2fd5018a5b01f6e10fe196a8f1a8cbf
jasonlotz <jasonlotz@gmail.com>
2025-02-02 12:57:12 -0500

    chore: upgrade plug-in versions

--- nvim/.config/nvim/lua/jason/plugins/kubectl.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    "ramilito/kubectl.nvim",
-    opt = true,
-    keys = {
-      { "<leader>k", "<cmd>lua require('kubectl').toggle()<cr>", desc = "Kubectl", noremap = true },
-    },
-  },
-}
