16a5eca829cd17ce19b0dd83873a00740ed2b8be
jasonlotz <jasonlotz@gmail.com>
2025-03-26 22:48:30 -0400

    chore: upgrade blink to v1

--- nvim/.config/nvim/lua/jason/plugins/blink.lua
+++ nvim/.config/nvim/lua/jason/plugins/blink.lua
@@ -2,15 +2,11 @@ return {
   "saghen/blink.cmp",
   lazy = false, -- lazy loading handled internally
   dependencies = "rafamadriz/friendly-snippets",
-  version = "v0.*",
+  version = "1.*",
   opts = {
     keymap = { preset = "default" },
 
     appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- Useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release
-      use_nvim_cmp_as_default = true,
       nerd_font_variant = "mono",
     },
 
@@ -19,9 +15,6 @@ return {
     sources = {
       default = { "lsp", "path", "snippets", "buffer" },
     },
-
-    -- experimental signature help support
-    signature = { enabled = true },
   },
   opts_extend = { "sources.default" },
 }
