880bfd13084ff77d6e5232cff2930b0ce8024a20
jasonlotz <jasonlotz@gmail.com>
2025-04-17 09:56:40 -0400

    feat: switch to new forked community edition of obsidian plug-in

--- nvim/.config/nvim/lua/jason/plugins/obsidian.lua
+++ nvim/.config/nvim/lua/jason/plugins/obsidian.lua
@@ -1,21 +1,13 @@
 return {
-  "epwalsh/obsidian.nvim",
+  "obsidian-nvim/obsidian.nvim",
   version = "*", -- recommended, use latest release instead of latest commit
-  lazy = false,
+  lazy = true,
   ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-  --   "BufReadPre path/to/my-vault/**.md",
-  --   "BufNewFile path/to/my-vault/**.md",
-  -- },
   dependencies = {
-    -- Required.
     "nvim-lua/plenary.nvim",
   },
   opts = {
-    disable_frontmatter = true,
+    -- disable_frontmatter = true,
     workspaces = {
       {
         name = "obsidian-vault",
