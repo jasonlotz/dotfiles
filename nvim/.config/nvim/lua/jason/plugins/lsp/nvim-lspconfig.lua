ab9e5a782ff128ff534f7cb4391abc7aa4a7928a
jasonlotz <jasonlotz@gmail.com>
2025-10-15 17:08:33 -0400

    feat: reverted back to custom diagnostic icons

--- nvim/.config/nvim/lua/jason/plugins/lsp/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/jason/plugins/lsp/nvim-lspconfig.lua
@@ -52,7 +52,12 @@ return {
         severity = { min = vim.diagnostic.severity.WARN },
       },
       signs = {
-        text = { "", "", "󰠠", "" },
+        text = {
+          [vim.diagnostic.severity.ERROR] = " ",
+          [vim.diagnostic.severity.WARN] = " ",
+          [vim.diagnostic.severity.HINT] = "󰠠 ",
+          [vim.diagnostic.severity.INFO] = " ",
+        },
       },
       virtual_text = true,
       float = {
