2749f987d246dd18393c96b48e7ca239ef490169
jasonlotz <jasonlotz@gmail.com>
2025-12-15 14:36:27 -0500

    feat: configs for ghostty terminal

--- stow-dotfiles.sh
+++ stow-dotfiles.sh
@@ -2,3 +2,4 @@ stow -v --dir=$HOME/Workspaces/dotfiles/nvim/ --target=$HOME/ --stow /
 stow -v --dir=$HOME/Workspaces/dotfiles/tmux/ --target=$HOME/ --stow / 
 stow -v --dir=$HOME/Workspaces/dotfiles/zsh/ --target=$HOME/ --stow / 
 stow -v --dir=$HOME/Workspaces/dotfiles/bin/ --target=/usr/local/bin/ --stow /
+stow -v --dir=$HOME/Workspaces/dotfiles/ghostty/ --target=$HOME/ --stow /
