6209c842edc14c8fba1ad1972ff188d1b0edf86e
jasonlotz <jasonlotz@gmail.com>
2025-12-16 10:26:34 -0500

    feat: fire cursor was a bit annoying actually

--- nvim/.config/nvim/lua/jason/plugins/smear-cursor.lua
+++ nvim/.config/nvim/lua/jason/plugins/smear-cursor.lua
@@ -1,24 +1,27 @@
 return {
   "sphamba/smear-cursor.nvim",
   opts = {
-    cursor_color = "#ff4000",
-    particles_enabled = true,
-    stiffness = 0.5,
-    trailing_stiffness = 0.2,
-    trailing_exponent = 5,
-    damping = 0.6,
-    gradient_exponent = 0,
-    gamma = 1,
-    never_draw_over_target = true, -- if you want to actually see under the cursor
-    hide_target_hack = true, -- same
-    particle_spread = 1,
-    particles_per_second = 500,
-    particles_per_length = 50,
-    particle_max_lifetime = 800,
-    particle_max_initial_velocity = 20,
-    particle_velocity_from_cursor = 0.5,
-    particle_damping = 0.15,
-    particle_gravity = -50,
-    min_distance_emit_particles = 0,
+    cursor_color = "#00ff00",
+
+    -- Fire hazard mode
+    -- cursor_color = "#ff4000",
+    -- particles_enabled = true,
+    -- stiffness = 0.5,
+    -- trailing_stiffness = 0.2,
+    -- trailing_exponent = 5,
+    -- damping = 0.6,
+    -- gradient_exponent = 0,
+    -- gamma = 1,
+    -- never_draw_over_target = true, -- if you want to actually see under the cursor
+    -- hide_target_hack = true, -- same
+    -- particle_spread = 1,
+    -- particles_per_second = 500,
+    -- particles_per_length = 50,
+    -- particle_max_lifetime = 800,
+    -- particle_max_initial_velocity = 20,
+    -- particle_velocity_from_cursor = 0.5,
+    -- particle_damping = 0.15,
+    -- particle_gravity = -50,
+    -- min_distance_emit_particles = 0,
   },
 }
