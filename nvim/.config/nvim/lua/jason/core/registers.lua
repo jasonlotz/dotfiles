5ceba31900d0fcae8abdebb49cb6d6aed568d964
jasonlotz <jasonlotz@gmail.com>
2026-02-23 12:21:52 -0500

    feat: update notes macro to start at the end of the buffer

--- nvim/.config/nvim/lua/jason/core/registers.lua
+++ nvim/.config/nvim/lua/jason/core/registers.lua
@@ -1,4 +1,4 @@
 # @n - Macro used for copying notes from Granola to Obsidian using my preferred formatting.
-local copy_notes_macro = vim.api.nvim_replace_termcodes("o<CR>jkpi#jkjjd6wjkGd3kjk", true, true, true)
+local copy_notes_macro = vim.api.nvim_replace_termcodes("Go<CR>jkpi#jkjjd6wjkGd3kjk", true, true, true)
 vim.fn.setreg("n", copy_notes_macro, "v")
 
